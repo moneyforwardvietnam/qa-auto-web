1ae1c966caab482806fdd0d7458df50ada22bf8d
White(Vu Bach) <bach.the.vu@moneyforward.vn>
2022-07-19 00:07:00 +0700

    QDS-01 scripting (1)

--- src/test/java/steps/LeftMenuSteps.java
+++ src/test/java/steps/LeftMenuSteps.java
@@ -15,6 +15,29 @@ public class LeftMenuSteps extends BaseStep {
 
     @And("User navigate to {string} on left menu")
     public void userNavigateToOnLeftMenu(String menuName) {
+        switch (menuName){
+            case "Settings":
+                menuName = "設定";
+                break;
+            case "Quotations":
+                menuName = "見積書";
+                break;
+            case "Opportunities":
+                menuName = "案件";
+                break;
+            case "Sales":
+                menuName = "売上";
+                break;
+            case "Invoices":
+                menuName = "請求書";
+                break;
+            case "Reconciliations":
+                menuName = "消込";
+                break;
+            case "Reports":
+                menuName = "レポート";
+                break;
+        }
         String xpath = "//ul[@class='el-menu']//child::span[contains(text(),'" + menuName + "')]//parent::li[1]";
         driverUtil.getElementByXPath(xpath).click();
     }
